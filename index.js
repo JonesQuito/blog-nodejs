d4c9c31ba6debc54a6501afb9f899cee338bb512
Oracle Public Cloud User <opc@web1.bastionnet.vcn1.oraclevcn.com>
2020-06-12 11:32:21 +0000

    update project

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const path = require('path');
 
+
 const express = require('express');
+
  
 const app = new express();
+
  
 app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 });
+
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
  
 app.listen(4000, () => {
     console.log('App listening on port 4000')
+});
+
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+ 
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
 });
